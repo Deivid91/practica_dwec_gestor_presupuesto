9589769ee540d96000df8c650530c1f92c090da6
David A. Sánchez <davidas91@hotmail.com>
2024-10-21 02:00:47 +0200

    Añadido repositorio remoto 'profesor', y modificado código gestionPresupuesto.js para que pase el test1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,44 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
+// Variable global
+let presupuesto = 0;
 
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(nuevoPresupuesto) {
+    if (nuevoPresupuesto >= 0) {
+        presupuesto = nuevoPresupuesto;
+        return presupuesto; 
+    }
+    else {
+        console.log("Error: Introduzca un número no negativo");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    this.valor = (typeof valor === 'number' && valor >= 0) ? valor : 0;
+
+    this.mostrarGasto = function() {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+
+    this.actualizarDescripcion = function(nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    };
+
+    this.actualizarValor = function(nuevoValor) {
+        if (typeof nuevoValor === 'number' && nuevoValor >= 0) {
+            this.valor = nuevoValor;
+        }
+        else {
+            console.log("Error: Introduzca un número no negativo.");
+        };
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
